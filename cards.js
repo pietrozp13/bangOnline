89e5c3ef3a164e9fd7461aa3f6cd8902ef17fb34
Pietrozp13 <pietro.perboni@softplan.com.br>
2020-03-30 02:21:48 -0300

    add use card

--- cards.js
+++ cards.js
@@ -1,58 +1,58 @@
 const bangCard = {
-    type: "usable",
-    descripiton: "Bang !!!",
-    mira: null
-}
+  type: "usable",
+  descripiton: "Bang !!!",
+  mira: null
+};
 
 const pistolaVulcCard = {
-    type: "disposable",
-    descripiton: "pistola vulcanic",
-    mira: 1
-}
+  type: "disposable",
+  descripiton: "pistola vulcanic",
+  mira: 1
+};
 
 const esquivaCard = {
-    type: "useble",
-    descripiton: "Esquiva",
-    mira: null
-}
+  type: "useble",
+  descripiton: "Esquiva",
+  mira: null
+};
 
 const allCards = [
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard,
-    bangCard,
-    pistolaVulcCard,
-    esquivaCard
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard,
+  bangCard,
+  pistolaVulcCard,
+  esquivaCard
 ];
 
-module.exports = allCards
+module.exports = allCards;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
 /*
     1 - cherife
@@ -14,148 +14,155 @@ var io = require('socket.io')(http);
     7 players = [1,2,2,7,7,7,9]
 */
 
-var allCards = require('./cards')
+var allCardsAll = require("./cards");
 
-var allPlayers = []
+var allCards = allCardsAll.map((card, index) => {
+  return {
+    ...card,
+    id: index
+  };
+});
 
-var allCardsPlayers = {}
+var allPlayers = [];
 
-var cardsPlay
+var allCardsPlayers = {};
+
+var cardsPlay;
 
 const allGamePlayers = {
-    "players4": [1,7,7,9],
-    "players5": [1,2,7,7,9],
-    "players6": [1,2,7,7,7,9],
-    "players7": [1,2,2,7,7,7,9]
-}
-
+  players4: [1, 7, 7, 9],
+  players5: [1, 2, 7, 7, 9],
+  players6: [1, 2, 7, 7, 7, 9],
+  players7: [1, 2, 2, 7, 7, 7, 9]
+};
 
 const getCharacters = () => {
-      let arr = allGamePlayers["players" + allPlayers.length]
-      arr.sort(() => Math.random() - 0.5);
-      return arr
-}
+  let arr = allGamePlayers["players" + allPlayers.length];
+  arr.sort(() => Math.random() - 0.5);
+  return arr;
+};
 
-io.on('connection', function(socket){
+io.on("connection", function(socket) {
+  socket.on("sendReadyStart", ({ id, name }) => {
+    const publicData = {
+      name: name,
+      id: id,
+      alive: true,
+      charatersType: null,
+      tableCards: [],
+      lifes: null
+    };
 
-    socket.on('sendReadyStart', ({id, name}) => {
-        const publicData = {
-            name: name,
-            id: id,
-            alive: true,
-            charatersType: null,
-            tableCards: [],
-            lifes: null,
-        }
+    allPlayers.push(publicData);
+  });
 
-        allPlayers.push(publicData)
+  socket.on("sendReadyStartMaster", () => {
+    const ramdomCharaters = getCharacters();
+    let firstPlayer;
+    let cards = allCards.sort(() => Math.random() - 0.5);
+
+    console.log(allPlayers);
+    allPlayers = allPlayers.map((element, index) => {
+      io.sockets.sockets[element.id].emit(
+        "user charater",
+        ramdomCharaters[index]
+      );
+      if (ramdomCharaters[index] === 1) {
+        firstPlayer = element.id;
+      }
+      return {
+        ...element,
+        id: element.id,
+        lifes: ramdomCharaters[index] === 1 ? 5 : 4
+      };
     });
 
+    allPlayers.map((element, index) => {
+      let myCards = [];
+      const cardsQuant = element.characterType === 1 ? 5 : 4;
 
-    socket.on('sendReadyStartMaster', () => {
-        const ramdomCharaters = getCharacters()
-        let firstPlayer
-        let cards = allCards.sort(() => Math.random() - 0.5);
+      for (let i = 0; i < cardsQuant; i++) {
+        myCards.push(cards[0]);
+        cards.shift();
+      }
 
-        console.log(allPlayers)
-        allPlayers = allPlayers.map((element, index) => {
-            io.sockets.sockets[element.id].emit("user charater", ramdomCharaters[index])
-            if (ramdomCharaters[index] === 1) {
-                firstPlayer = element.id
-            }
-            return {
-                ...element,
-                id: element.id,
-                lifes: ramdomCharaters[index] === 1 ? 5 : 4
-            }
-        });
-
-        allPlayers.map((element, index) => {
-            let myCards = []
-            const cardsQuant = element.characterType === 1 ? 5 : 4
-
-            for(let i = 0; i < cardsQuant; i++){
-                myCards.push(cards[0])
-                cards.shift()
-            }
-
-            allCardsPlayers = {
-                ...allCardsPlayers,
-                [element.name]: myCards
-            }
-            io.sockets.sockets[element.id].emit("user cards", myCards)
-        });
-
-        cardsPlay = cards
-
-        // start Game
-        io.emit('AllGameData', allPlayers);
-
-
-        io.emit("playerTurn", {
-            playerID: firstPlayer
-        })
+      allCardsPlayers = {
+        ...allCardsPlayers,
+        [element.name]: myCards
+      };
+      io.sockets.sockets[element.id].emit("user cards", myCards);
     });
 
+    cardsPlay = cards;
 
-    socket.on('debug', () => {
-        io.emit('debugC', {
-            allPlayers,
-            allCardsPlayers,
-            cardsPlay
-        });
+    // start Game
+    io.emit("AllGameData", allPlayers);
+
+    io.emit("playerTurn", {
+      playerID: firstPlayer
     });
+  });
 
-    socket.on('handlePassTurn', ({ playerID }) => {
-        
-        let currentIndexPlayer = allPlayers.findIndex((item)=> item.id === playerID)
- 
-        
-        if (currentIndexPlayer === allPlayers.length - 1) {
-            currentIndexPlayer = 0
-        } else {
-            currentIndexPlayer++
-        }
-
-        io.emit("playerTurn", {
-            playerID: allPlayers[currentIndexPlayer].id
-        })
+  socket.on("debug", () => {
+    io.emit("debugC", {
+      allPlayers,
+      allCardsPlayers,
+      cardsPlay
     });
+  });
 
+  socket.on("handlePassTurn", ({ playerID }) => {
+    let currentIndexPlayer = allPlayers.findIndex(item => item.id === playerID);
 
+    if (currentIndexPlayer === allPlayers.length - 1) {
+      currentIndexPlayer = 0;
+    } else {
+      currentIndexPlayer++;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-    socket.on('message', function(msg){
-        console.log("msg recebida", msg)
-        io.emit('message', msg);
+    io.emit("playerTurn", {
+      playerID: allPlayers[currentIndexPlayer].id
     });
+  });
 
-    socket.on('private message', function(msg){
-        console.log("prev msg recebida", msg)
-        io.emit('message', msg);
-    });
+  socket.on("Use Cart", data => {
+    console.log("dados use card", data);
+  });
 
-    // ------------
-    socket.on('disconnect', function(){
-        console.log('Disconnected!!!');
-        var allPlayers = []
+  /*
+-
+-
+-
+--
+-
+-
+-
+-
+-
+--
+*/
 
-        var allCardsPlayers = {}
+  socket.on("message", function(msg) {
+    console.log("msg recebida", msg);
+    io.emit("message", msg);
+  });
 
-        var cardsPlay
+  socket.on("private message", function(msg) {
+    console.log("prev msg recebida", msg);
+    io.emit("message", msg);
+  });
+
+  // ------------
+  socket.on("disconnect", function() {
+    console.log("Disconnected!!!");
+    var allPlayers = [];
+
+    var allCardsPlayers = {};
+
+    var cardsPlay;
   });
 });
 
-http.listen(5099, function(){
-  console.log('listening on *:5099');
+http.listen(5099, function() {
+  console.log("listening on *:5099");
 });
